7e211b612906474bba4fe23d93fe0dbd5aeb243a
ArtifaX <reutov.alexey@yahoo.com>
2014-11-26 16:45:59 +0000

    Lets hope...

--- src/se/sics/tac/aw/handlers/FlightHandler.java
+++ src/se/sics/tac/aw/handlers/FlightHandler.java
@@ -10,12 +10,12 @@ public class FlightHandler extends Handler {
 	
 	int[][] auctionHistory;
 	int timeInterval;								//current time interval
-	double trend;
+	double[] trend;
 	
 	public FlightHandler(TACAgent agent) {
-		
+		///hm..
 		auctionHistory = new int[8][55]; 		//price history
-
+		trend = new double[8];
 		this.agent = agent;
 		
 		//For all flight auctions (0-7), initialize to -1
@@ -24,54 +24,64 @@ public class FlightHandler extends Handler {
 				agent.setBidder(j, i, -1);
 			}
 		}
+		
 		timeInterval = (int) agent.getGameTime()/10000;		//Try to get how many 10s has passed since the beginning
 		for (int i=0; i<8; i++)
 		{
-			auctionHistory[i][timeInterval]=(int) agent.getQuote(i).getAskPrice();				//?????Current auction price... Not sure how to get that...=/
+			auctionHistory[i][timeInterval]=(int) agent.getQuote(i).getAskPrice();		//Hope it works
+		}
+		for (int i=0; i<8; i++)
+		{
+			trend[i]=TrendCalc(i);
 		}
 		
 	}
 	
-	
+	void Initialisation ()
+	{
+		//Initialise auctionHistory matrix
+				for (int i=0; i<8; i++)				
+				{
+					for (int j=0; j<55; j++)
+					{
+						auctionHistory[i][j]=0;
+					}
+				}
+	}	
 	
 	//       *******************Trend prediction (CAUTION!! A lot of mess detected!! Brace yourself b4 proceeding!!)*********************************
 	
-	void Solver(double [][]values, double w, double c)
+	double[] Solver(double [][]values, double w, double c)
 {
     double delta = values[0][0] * values[1][1] - values[1][0] * values[0][1];
     double delta_w = values[0][1] * values[1][2] - values[1][1] * values[0][2];
     double delta_c = values[0][0] * values[1][2] - values[1][0] * values[0][2];
+    double[] param = new double [2];
     c = -delta_w / delta;
     w = delta_c / delta;
-	//c=values[1][0]*values[0][2]/(values[1][0]*values[0][1]+values[1][1])-values[1][2]*values[0][0]/(values[1][0]*values[0][1]+values[1][1]);
-	//w=values[0][2]/values[0][0]-values[0][1]/values[0][0]*c;
+	param[0]=w;
+	param[1]=c;
+	return param;
 }
  
-double TrendCalc()												// Calculates and returns inclanation
+	double TrendCalc(int auction)												// Calculates and returns inclanation
 {
-    int[][] PriceMat = new int[8][55];				//should be our auctionHistory from above. 
+    //int[][] PriceMat = new int[8][55];				//should be our auctionHistory from above. 
 	int s=0;										//counter of how many idexies are already filled
 	double[][] values = new double[2][3];			//Equation solver values
-	double[][] EMat = new double[8][2];				//matrix of linear parameters for all auctions
+	double[] EMat = new double[2];				//matrix of linear parameters for all auctions
 
 
-	for (int i=0; i<8; i++)				
-		{
-			for (int j=0; j<55; j++)
-			{
-				PriceMat[i][j]=0;
-			}
-		}
-
+	
 	//  Test filling
-	PriceMat[0][0]=219;
-	PriceMat[0][1]=231;
-	PriceMat[0][2]=228;
-	PriceMat[0][3]=233;
-	PriceMat[0][4]=245;
-	PriceMat[0][5]=257;
-	PriceMat[0][6]=254;
-	PriceMat[0][7]=265;
+	auctionHistory[0][0]=219;
+	auctionHistory[0][1]=231;
+	auctionHistory[0][2]=228;
+	auctionHistory[0][3]=233;
+	auctionHistory[0][4]=245;
+	auctionHistory[0][5]=257;
+	auctionHistory[0][6]=254;
+	auctionHistory[0][7]=265;
 
 
 
@@ -79,15 +89,13 @@ double TrendCalc()												// Calculates and returns inclanation
 
 	for (int i=0; i<55; i++) 			//finding how many values we have
 	{
-		if (PriceMat[0][i]==0)
+		if (auctionHistory[0][i]==0)
 		{
 			s=i;
 			break;						
 		}
 	}
 
-	for (int k=0; k<1; k++)
-	{
 		double w = 0;
 		double c = 0;
 		// Filing out our values									
@@ -104,22 +112,20 @@ double TrendCalc()												// Calculates and returns inclanation
 		for (int i=0; i<s; i++)
 		{
 			values[0][1]+=i+1;
-			values[0][2]+=PriceMat[k][i];
+			values[0][2]+=auctionHistory[auction][i];
 			values[1][0]+=i+1;
 			values[1][1]+=(i+1)*(i+1);
-			values[1][2]+=PriceMat[k][i]*(i+1);
+			values[1][2]+=auctionHistory[auction][i]*(i+1);
 		}
 		//values[1][2]*=s;
 
-		Solver(values, w, c);								//Not sure how to convert that one
-		EMat[k][0]=w;
-		EMat[k][1]=c;
-
-		
+		EMat=Solver(values, w, c);								//Not sure how to convert that one
+		//EMat[client][0]=w;
+		//EMat[client][1]=c;
+		//trend[auction];
+		return EMat[0];
 	}
 
-	return EMat[0][0];										// we actually need only inclanation, not initial shift
-}
 	
 
 	
@@ -137,8 +143,8 @@ double TrendCalc()												// Calculates and returns inclanation
 		for (int client=0; client < 8 ; client++){
 			//If the client wants something from this auction
 			if (agent.getBidder(auction, client) != -1) {
-				if (auctionHistory[client][timeInterval]>450 || trend>5)   	//if the price is above 450 and we still need it - buy b4 too late
-				{															//or if it climbs too fast - do the same
+				if (auctionHistory[client][timeInterval]>450 || trend[auction]>5)   	//if the price is above 450 and we still need it - buy b4 too late
+				{																		//or if it climbs too fast - do the same
 					//TODO Put here the calculation for the price
 					//int utilityPackage = packageSet.get(client).getUtility();
 					int price = auctionHistory[client][timeInterval]; 			// current auction price
